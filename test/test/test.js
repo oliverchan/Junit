deccf3f19059e90a4529e68478aede024751d387
oliver chan <oliverchanyk@gmail.com>
2021-06-30 20:01:00 +0800

    Add files via upload

---
+++ test/test/test.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+// describe("Array", function () {
+//   describe("#indexOf()", function () {
+//     it("should return -1 when the value is not present", function () {
+//       assert.equal(-1, [1, 2, 3].indexOf(4));
+//     });
+//   });
+// });
+describe("test suite 1", () => {
+  it("test case 1", function () {
+    // do something
+    // assert something
+  });
+});
+describe("test suite 2", () => {
+  it("test case 2", function () {
+    // do something
+    // assert something
+  });
+});
+describe("test suite 3", () => {
+  it("test case 6", function () {
+    // do something
+    // assert something
+  });
+});
