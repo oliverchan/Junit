a4f3b1addcb86fbcc7793644f9d2f8eafeb57a55
oliver chan <oliverchanyk@gmail.com>
2021-07-04 13:06:25 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,24 +6,24 @@ var assert = require("assert");
 //     });
 //   });
 // });
-describe("To connect with test result with jira stories-4x", () => {
-  it("pipeline - test case 31", function () {
+describe("To connect with test result with jira stories-5x", () => {
+  it("pipeline - test case 41", function () {
     // do something
     // assert something
   })
-  it("pipeline - should show test case 32", function () {
+  it("pipeline - should show test case 42", function () {
     // do something
     // assert something
   })
   
 });
-describe("test suite 12", () => {
+describe("test suite 43", () => {
   it("pipeline test case 12", function () {
     // do something
     // assert something
   });
 });
-describe("test suite 3", () => {
+describe("test suite 44", () => {
   it("pipeline test case 9", function () {
     // do something
     // assert something
